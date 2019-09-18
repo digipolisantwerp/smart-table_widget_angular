7dbc5f38fb120dc0779ce6a80b20b63a4907397a
Tom Ceuppens <t-c@live.be>
2019-09-18 17:20:21 +0200

    translations + extra readme info

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppRatingComponent } from './rating.component';
+import { SmartTableConfig } from 'projects/smart-table/src/lib/smart-table/smart-table.types';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,14 @@ export class AppComponent {
     name: 'rating',
     component: AppRatingComponent
   }];
+
+  customConfiguration ={
+    translations: {
+      moreFilters: 'More filters',
+      export: 'Download',
+      apply: 'Apply'
+    }
+  };
 
   onRowClicked(row) {
     console.log('clicked row', row);
