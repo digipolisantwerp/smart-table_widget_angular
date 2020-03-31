da2ac3ce02a897369f6f450b1039fcd88b14aacb
Michael Castiau <castiau.m@gmail.com>
2020-03-31 11:57:35 +0200

    Use reactive forms io forms

--- projects/smart-table/src/lib/filter/abstract-filter.ts
+++ projects/smart-table/src/lib/filter/abstract-filter.ts
@@ -1,8 +1,10 @@
-import { EventEmitter, Input, Output } from '@angular/core';
+import {EventEmitter, Input, OnDestroy, Output} from '@angular/core';
 
-import { SmartTableFilter, UpdateFilterArgs } from './../smart-table/smart-table.types';
+import {SmartTableFilter, UpdateFilterArgs} from '../smart-table/smart-table.types';
+import {FormControl} from '@angular/forms';
+import {Subject} from 'rxjs';
 
-export abstract class AbstractFilter {
+export abstract class AbstractFilter implements OnDestroy {
   @Input() filter: SmartTableFilter;
   @Input() optional = false;
   @Output() update = new EventEmitter<UpdateFilterArgs>();
@@ -10,8 +12,16 @@ export abstract class AbstractFilter {
   public id: string;
   public value: string;
 
+  public formControl: FormControl = new FormControl();
+  public destroy$ = new Subject();
+
   public onFilter(value) {
     this.filter.value = value;
-    this.update.emit({ filter: this.filter, value });
+    this.update.emit({filter: this.filter, value});
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- projects/smart-table/src/lib/table-select-filter/table-select-filter.component.ts
+++ projects/smart-table/src/lib/table-select-filter/table-select-filter.component.ts
@@ -1,22 +1,28 @@
-import { FilterComponent } from '@acpaas-ui/ngx-components/utils';
-import { Component, OnInit } from '@angular/core';
-import _ from 'lodash';
-
-import { AbstractFilter } from '../filter/abstract-filter';
+import {FilterComponent} from '@acpaas-ui/ngx-components/utils';
+import {Component, OnInit} from '@angular/core';
+import {AbstractFilter} from '../filter/abstract-filter';
+import {SmartTableFilter} from '../filter/filter.decorator';
 
 @Component({
   selector: 'aui-table-select-filter',
-  templateUrl: './table-select-filter.component.html'
+  templateUrl: './table-select-filter.component.html',
+  styles: [':host {flex-grow: 1}']
 })
+@SmartTableFilter()
 export class TableSelectFilterComponent extends AbstractFilter implements OnInit, FilterComponent {
 
   public ngOnInit() {
-    this.id = `filter-${this.filter.id}-${_.uniqueId()}`;
-    if (this.filter && _.isString(this.filter.value)) {
-      this.value = this.filter.options.find(option => option && option.value === this.filter.value);
+    try {
+      if (this.filter && (typeof this.filter.value === 'string')) {
+        const foundOption = this.filter.options.find(option => option.id.toLowerCase() === (this.filter.value as string).toLowerCase());
+        if (foundOption) {
+          this.formControl.setValue(foundOption.id);
+        }
+      }
+    } catch (err) {
+      console.error(err);
+      console.warn('Warning: could not set select filter value.');
     }
-    if (this.filter && this.filter.placeholder) {
-      this.value = '';
-    }
+
   }
 }
