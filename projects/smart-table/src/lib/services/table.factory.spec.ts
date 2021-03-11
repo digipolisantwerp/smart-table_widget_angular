cc0c37968a3d4f84579e25c5ebf50d45919abdb8
Jochen Schoubben <jochen.schoubben@digipolis.be>
2021-03-11 16:28:29 +0100

    Added date(Time) string value tests

--- projects/smart-table/src/lib/services/table.factory.spec.ts
+++ projects/smart-table/src/lib/services/table.factory.spec.ts
@@ -31,6 +31,30 @@ describe('Table Factory Test', () => {
       const d = new Date(2019, 8, 10, 6, 23);
       expect(column.format(d as any)).toBe('10/09/2019');
     });
+    it('should create a date column in date format for date string, default component', () => {
+      const column = factory.createTableColumnFromConfig({
+        visible: true,
+        label: 'test-column',
+        key: 'test-key',
+        type: SmartTableColumnType.Date,
+        sortPath: 'none'
+      }, []);
+      // Now let's format a date
+      const d = '2020-08-10';
+      expect(column.format(d as any)).toBe('10/08/2020');
+    });
+    it('should create a date column with empty value for invalid date string, default component', () => {
+      const column = factory.createTableColumnFromConfig({
+        visible: true,
+        label: 'test-column',
+        key: 'test-key',
+        type: SmartTableColumnType.Date,
+        sortPath: 'none'
+      }, []);
+      // Now let's format a date
+      const d = 'ABC';
+      expect(column.format(d as any)).toBe('');
+    });
     it('should create a date column in datetime format, default component', () => {
       const column = factory.createTableColumnFromConfig({
         visible: true,
@@ -42,6 +66,31 @@ describe('Table Factory Test', () => {
       // Now let's format a date
       const d = new Date(2019, 8, 10, 6, 23);
       expect(column.format(d as any)).toBe('10/09/2019 - 06:23');
+    });
+    it('should create a dateTime column in datetime format for datetime string, default component', () => {
+      const column = factory.createTableColumnFromConfig({
+        visible: true,
+        label: 'test-column',
+        key: 'test-key',
+        type: SmartTableColumnType.DateTime,
+        sortPath: 'none'
+      }, []);
+      // Now let's format a date
+      const d = '2020-08-10T08:13:13.584Z';
+      const date = new Date('2020-08-10T08:13:13.584Z');
+      expect(column.format(d as any)).toBe(`10/08/2020 - ${date.getHours().toString().padStart(2, '0')}:13`);
+    });
+    it('should create a dateTime column with empty value for invalid datetime string, default component', () => {
+      const column = factory.createTableColumnFromConfig({
+        visible: true,
+        label: 'test-column',
+        key: 'test-key',
+        type: SmartTableColumnType.DateTime,
+        sortPath: 'none'
+      }, []);
+      // Now let's format a date
+      const d = 'ABC';
+      expect(column.format(d as any)).toBe('');
     });
     it('should create a date column and take into account optinos, default component', () => {
       const column = factory.createTableColumnFromConfig({
