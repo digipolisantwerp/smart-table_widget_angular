bbfdcac63d4405d86e25c3b6e9069829322cd670
Indy De Wacker <indy.dewacker@gmail.com>
2022-02-03 11:14:16 +0100

    fix: Entry point '@acpaas-ui-widgets/ngx-smart-table' contains deep imports into 'localstorage.types'

--- projects/smart-table/src/lib/providers/indentifier.provider.ts
+++ projects/smart-table/src/lib/providers/indentifier.provider.ts
@@ -1,6 +1,7 @@
+import { ILocalStorageConfig } from '@acpaas-ui/ngx-localstorage';
 import { InjectionToken } from '@angular/core';
+
 import { IModuleConfig } from '../smart-table.types';
-import { ILocalStorageConfig } from '@acpaas-ui/ngx-localstorage/lib/types/localstorage.types';
 
 export const PROVIDE_ID = new InjectionToken('Provide-Identifier');
 export const PROVIDE_CONFIG = new InjectionToken('SmartTable-Provide-Config');
